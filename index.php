dae161974485b75419b9a32818883a37a0cf3277
fukashida <114131100+fukashida@users.noreply.github.com>
2026-03-03 10:31:45 +0900

    性感染症検査・治療ページ追加

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     // 共通URL
     $visit_url  = 'https://aposuru.com/salon/salon_schedules/create/fab37cc0-ddfe-44f9-9023-7772489e2b1f';
     $online_url = 'https://femuse-clinic.jp/online/';
+    $std_url = 'https://femuse-clinic.jp/std-test/';
 
     // 親ページ（/menu/）を取得
     $menu_parent = get_page_by_path('menu');
@@ -40,14 +41,15 @@
       <div class="container">
         <div class="menu-grid">
           <?php foreach ($children as $child): ?>
-            <?php
-              $has_online = function_exists('get_field') ? get_field('has_online', $child->ID) : null;
 
-              $enabled =
-                $has_online === true ||
-                $has_online === 1 ||
-                $has_online === '1' ||
-                (is_array($has_online) && in_array('1', $has_online, true));
+            <?php
+              $has_online   = get_field('has_online', $child->ID);
+              $has_athome   = get_field('has_athome', $child->ID);
+              $has_atclinic = get_field('has_atclinic', $child->ID);
+
+              $online_enabled   = is_array($has_online) && in_array('1', $has_online, true);
+              $athome_enabled   = is_array($has_athome) && in_array('1', $has_athome, true);
+              $atclinic_enabled = is_array($has_atclinic) && in_array('1', $has_atclinic, true);
             ?>
 
             <div class="menu-card">
@@ -57,15 +59,29 @@
               </a>
 
               <div class="menu-card__actions">
-                <?php if ($enabled): ?>
-                  <a target=”_blank” class="btn btn--primary" href="<?php echo esc_url($online_url); ?>">
+
+                <?php if ($online_enabled): ?>
+                  <a target="_blank" class="btn btn--primary" href="<?php echo esc_url($online_url); ?>">
                     オンライン診療予約
                   </a>
                 <?php endif; ?>
 
-                <a target=”_blank” class="btn btn--secondary" href="<?php echo esc_url($visit_url); ?>">
-                  来院予約
-                </a>
+                <?php if ($athome_enabled): ?>
+                  <a target="_blank" class="btn btn--primary" href="<?php echo esc_url($std_url); ?>">
+                    自宅で検査
+                  </a>
+                <?php endif; ?>
+
+                <?php if ($atclinic_enabled): ?>
+                  <a target="_blank" class="btn btn--secondary" href="<?php echo esc_url($visit_url); ?>">
+                    クリニックで検査
+                  </a>
+                <?php else: ?>
+                  <a target="_blank" class="btn btn--secondary" href="<?php echo esc_url($visit_url); ?>">
+                    来院予約
+                  </a>
+                <?php endif; ?>
+
               </div>
             </div>
 
