35638f2aa4090fdf5081bf17f37aea355bec7f3d
fukashida <114131100+fukashida@users.noreply.github.com>
2026-05-14 10:20:40 +0900

    5月カレンダー追加

---
+++ parts/menu-online-modal.php
@@ -0,0 +1,52 @@
+<div id="modal-menu-online" class="modal-container">
+  <div class="modal-menu-online-close">×</div>
+  <div class="modal-body">
+    <!-- <div class="modal-content"> -->
+    <div class="modal-menu-online-container">
+      <h2 class="modal-menu-online-title"><span>オンライン診療</span></h2>
+      <div class="modal-menu-online-list-area">
+        <div class="modal-menu-online-list">
+          <div class="modal-menu-online-list-title">
+            低用量ピル
+          </div>
+          <div class="modal-menu-online-list-btn">
+            <a href="https://reserve-femuse-clinic.b4a.clinic/clinics/234/bookings/new/select/date/?i=108174" target="_blank" rel="noopener noreferrer">
+              予約する
+            </a>
+          </div>
+        </div>
+        <div class="modal-menu-online-list">
+          <div class="modal-menu-online-list-title">
+            月経移動
+          </div>
+          <div class="modal-menu-online-list-btn">
+            <a href="https://reserve-femuse-clinic.b4a.clinic/clinics/234/bookings/new/select/date/?i=108180" target="_blank" rel="noopener noreferrer">
+              予約する
+            </a>
+          </div>
+        </div>
+        <div class="modal-menu-online-list">
+          <div class="modal-menu-online-list-title">
+            美白内服セット
+          </div>
+          <div class="modal-menu-online-list-btn">
+            <a href="https://reserve-femuse-clinic.b4a.clinic/clinics/234/bookings/new/select/date/?i=108167" target="_blank" rel="noopener noreferrer">
+              予約する
+            </a>
+          </div>
+        </div>
+        <div class="modal-menu-online-list">
+          <div class="modal-menu-online-list-title">
+            美肌内服セット
+          </div>
+          <div class="modal-menu-online-list-btn">
+            <a href="https://reserve-femuse-clinic.b4a.clinic/clinics/234/bookings/new/select/date/?i=108169" target="_blank" rel="noopener noreferrer">
+              予約する
+            </a>
+          </div>
+        </div>
+        <!-- </div> -->
+      </div>
+    </div>
+  </div>
+</div>
---
+++ parts/reserve-modal.php
@@ -0,0 +1,45 @@
+<div id="modal-call" class="modal-container">
+  <div class="modal-call-close">×</div>
+  <div class="modal-body">
+    <div class="modal-content">
+      <div class="modal-call-container">
+        <div class="modal-call-title pc-disp"><span>電話からご予約</span></div>
+        <div class="modal-call-title sp-disp"><a href="tel:03-6205-5937"><span>電話からご予約</span></a></div>
+        <div class="modal-call-phone">03-6205-5937</div>
+        <div class="modal-reserve-day">
+          <img src="<?php my_theme_uri(); ?>/assets/images/top/pc_business_day.png" />
+          <span>※月曜、年末年始休み</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="modal-line" class="modal-container">
+  <div class="modal-line-close">×</div>
+  <div class="modal-body">
+    <div class="modal-line-container">
+      <div class="modal-line-title"><span>LINEからの予約手順</span></div>
+      <div class="modal-line-icon-area">
+        <div class="modal-line-icon-img">
+          <img src="<?php my_theme_uri(); ?>/assets/images/top/line_icon.png" />
+        </div>
+        <p>Femuse clinic<br />公式LINEアカウント</p>
+      </div>
+      <div class="modal-line-step">
+        <img src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_1.png" /> スマホでLINEアプリを立ち上げ
+      </div>
+      <img class="modal-line-arrow" src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_arrow.png" />
+      <div class="modal-line-step">
+        <img src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_2.png" /> Femuse clinicを友達追加
+      </div>
+      <img class="modal-line-arrow" src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_arrow.png" />
+      <div class="modal-line-step">
+        <img src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_3.png" /> 「予約」をクリック
+      </div>
+      <img class="modal-line-arrow" src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_arrow.png" />
+      <div class="modal-line-step">
+        <img src="<?php my_theme_uri(); ?>/assets/images/top/pc_line_4.png" /> 「新規予約」からご予約
+      </div>
+    </div>
+  </div>
+</div>
